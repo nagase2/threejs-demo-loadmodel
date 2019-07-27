2fa0c8e18792310c107c75eead1acf26cdcebe23
nagase2 <nagase2@gmail.com>
2019-07-27 21:01:29 +0900

    1 file modified

--- index.js
+++ index.js
@@ -1,86 +1,13 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import * as THREE from "three";
+
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
+import Stats from "three/examples/jsm/libs/stats.module.js";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader";
 
-import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader';
-
-
-var container, stats, controls;
-var camera, scene, renderer, light;
-var clock = new THREE.Clock();
-var mixer;
- init();
- //animate();
-function init() {
-  container = document.createElement( 'div' );
-  document.body.appendChild( container );
-  camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 2000 );
-  camera.position.set( 100, 200, 300 );
-  scene = new THREE.Scene();
-  scene.background = new THREE.Color( 0xa0a0a0 );
-  scene.fog = new THREE.Fog( 0xa0a0a0, 200, 1000 );
-  light = new THREE.HemisphereLight( 0xffffff, 0x444444 );
-  light.position.set( 0, 200, 0 );
-  scene.add( light );
-  light = new THREE.DirectionalLight( 0xffffff );
-  light.position.set( 0, 200, 100 );
-  light.castShadow = true;
-  light.shadow.camera.top = 180;
-  light.shadow.camera.bottom = - 100;
-  light.shadow.camera.left = - 120;
-  light.shadow.camera.right = 120;
-  scene.add( light );
-  //scene.add( new CameraHelper( light.shadow.camera ) );
-  // ground
-  var mesh = new THREE.Mesh( new THREE.PlaneBufferGeometry( 2000, 2000 ), new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: false } ) );
-  mesh.rotation.x = - Math.PI / 2;
-  mesh.receiveShadow = true;
-  scene.add( mesh );
-  var grid = new THREE.GridHelper( 2000, 20, 0x000000, 0x000000 );
-  grid.material.opacity = 0.2;
-  grid.material.transparent = true;
-  scene.add( grid );
-  // model
-  var loader = new FBXLoader();
-  loader.load( 'models/fbx/nurbs.fbx', function ( object ) {
-    mixer = new THREE.AnimationMixer( object );
-    var action = mixer.clipAction( object.animations[ 0 ] );
-    action.play();
-    object.traverse( function ( child ) {
-      if ( child.isMesh ) {
-        child.castShadow = true;
-        child.receiveShadow = true;
-      }
-    } );
-    scene.add( object );
-  } );
-  renderer = new THREE.WebGLRenderer( { antialias: true } );
-  renderer.setPixelRatio( window.devicePixelRatio );
-  renderer.setSize( window.innerWidth, window.innerHeight );
-  renderer.shadowMap.enabled = true;
-  container.appendChild( renderer.domElement );
-  controls = new OrbitControls( camera, renderer.domElement );
-  controls.target.set( 0, 100, 0 );
- // controls.update();
-  window.addEventListener( 'resize', onWindowResize, false );
-  // stats
-  // stats = new Stats();
-  // container.appendChild( stats.dom );
-}
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize( window.innerWidth, window.innerHeight );
-}
-//
-function animate() {
-  requestAnimationFrame( animate );
-  var delta = clock.getDelta();
-  if ( mixer ) mixer.update( delta );
-  renderer.render( scene, camera );
-  stats.update();
-}
+import { ColladaLoader } from "three/examples/jsm/loaders/ColladaLoader.js";
 
 const style = {
   height: 400 // we can control scene size by setting container dimensions
@@ -88,10 +15,8 @@ const style = {
 
 class App extends Component {
   silenState = { isMounted: true };
- 
 
   componentDidMount() {
-    console.log(FBXLoader);
     this.sceneSetup();
     this.addCustomSceneObjects();
     this.startAnimationLoop();
@@ -122,13 +47,51 @@ class App extends Component {
       0.1, // near plane
       1000 // far plane
     );
-    this.camera.position.z = 5; // is used here to set some distance from a cube that is located at z = 0
+
+    this.camera.position.set( 4, 5, 4);
+		//this.camera.lookAt( 0, 2, 0 );
+
+    //this.camera.position.z = 10; // is used here to set some distance from a cube that is located at z = 0
     // OrbitControls allow a camera to orbit around the object
     // https://threejs.org/docs/#examples/controls/OrbitControls
     this.controls = new OrbitControls(this.camera, this.el);
     this.renderer = new THREE.WebGLRenderer();
     this.renderer.setSize(width, height);
     this.el.appendChild(this.renderer.domElement); // mount using React ref
+
+    //地面を表示
+      var mesh = new THREE.Mesh( new THREE.PlaneBufferGeometry( 2000, 2000 ), new THREE.MeshPhongMaterial( { color: 0x999999, depthWrite: false } ) );
+    mesh.rotation.x = - Math.PI / 2;
+    mesh.receiveShadow = true;
+    this.scene.add( mesh );
+    var grid = new THREE.GridHelper( 2000, 1000, 0x000000, 0x000000 );
+    grid.position.set(0,-1,0)
+    grid.material.opacity = 0.2;
+    grid.material.transparent = true;
+    this.scene.add( grid );
+
+    this.scene.fog = new THREE.Fog( 0xa0a0a0, 3, 33 );
+
+      //collada elfモデル
+    var loadingManager = new THREE.LoadingManager(function() {
+      this.scene.add(elf);
+    });
+    // var colladaLoader = new ColladaLoader(loadingManager);
+    // colladaLoader.load("models/collada/stormtrooper/stormtrooper.dae", function(collada) {
+    //     collada.scene.traverse(function(child) {
+    //         if (child.isMesh) {
+    //           child.castShadow = true;
+    //           child.receiveShadow = true;
+    //          // child.position.x=20
+    //         }
+    //       });
+    //   elf = collada.scene;
+    //   elf.position.set(21,13,-33)
+    // });
+
+
+  
+  
   };
 
  
@@ -136,17 +99,20 @@ class App extends Component {
     //救急車本体部分
     const geometry = new THREE.BoxGeometry(5, 2, 2);
 
-    const material = new THREE.MeshPhongMaterial({
+    this.material = new THREE.MeshPhongMaterial({
       color: 0xffffff,
       emissive: 0x072534,
+      transparent: true,
+      opacity : 0.95, //透明度
+      wireframe :false, //ワイヤフレーム
       side: THREE.DoubleSide,
       flatShading: true
     });
-    this.cube = new THREE.Mesh(geometry, material);
+    this.cube = new THREE.Mesh(geometry, this.material);
     this.scene.add(this.cube);
 
     const geometory2 = new THREE.BoxGeometry(0.8, 1.5, 1.5);
-    this.cube2 = new THREE.Mesh(geometory2,material);
+    this.cube2 = new THREE.Mesh(geometory2,this.material);
     this.cube2.position.set(-2.8,-0.2,0)
     this.scene.add(this.cube2);
 
@@ -187,6 +153,16 @@ class App extends Component {
     lights[0].position.set(1, 100, 0);
     lights[1].position.set(100, 0, 100);
     lights[2].position.set(-10, -10, -10);
+    
+
+    let light = new THREE.HemisphereLight(0x555555, 0x111111);
+    light.position.set(333, 200, 33);
+    this.scene.add(light);
+    light.castShadow =true;
+    // light.shadow.camera.top = 18;
+    // light.shadow.camera.bottom = - 10;
+    // light.shadow.camera.left = - 10;
+    // light.shadow.camera.right = 120;
 
     this.scene.add(lights[0]);
     this.scene.add(lights[1]);
@@ -232,12 +208,15 @@ class App extends Component {
     this.cube21 = new THREE.Mesh(geometry21, material);
     this.cube21.name = objName
     this.cube21.position.set(2, 1, 0);
+    this.cube21.castShadow =true;
     this.scene.add(this.cube21);
 
     const geometry22 = new THREE.BoxGeometry(0.8, 0.5, 1);
     this.cube22 = new THREE.Mesh(geometry22, material);
     this.cube22.name = objName
     this.cube22.position.set(-2, 1, 0);
+    this.cube22.castShadow =true;
+    this.cube22.receiveShadow=true;
     this.scene.add(this.cube22);
   }
   //サイレンBを追加する
@@ -284,7 +263,9 @@ class App extends Component {
     this.cube22.position.set(-2, 1, 0);
     this.scene.add(this.cube22);
   }
-  
+  changeWireframeSetting(){
+    this.material.wireframe ? this.material.wireframe =false : this.material.wireframe= true;
+  }
   //指定されたオブジェクトを削除
   removeParts(objName){
     // console.log("remove")
@@ -297,7 +278,6 @@ class App extends Component {
     
   }
   render() {
-    
     return (
       <div style={style} ref={ref => (this.el = ref)}>
         <button
@@ -328,7 +308,14 @@ class App extends Component {
         >
         サイレン消す
         </button>
-     
+        <button
+          onClick={() =>
+            this.changeWireframeSetting
+        ()
+          }
+        >
+        ワイヤフレーム
+        </button>    
      
         <br/><br/>
       </div>
